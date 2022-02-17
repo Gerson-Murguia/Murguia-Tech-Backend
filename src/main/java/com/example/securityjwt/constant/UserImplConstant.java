a2aac83ffa11735364584c1fcb89cc2f84fe4c34
GersonMurguia <aldmurguia1@gmail.com>
2022-02-17 13:03:12 -0500

    Cambio de constante

--- src/main/java/com/example/securityjwt/constant/UserImplConstant.java
+++ src/main/java/com/example/securityjwt/constant/UserImplConstant.java
@@ -3,7 +3,7 @@ package com.example.securityjwt.constant;
 public class UserImplConstant {
 
 
-    public static final String EMAIL_YA_EXISTE = "Email ya existe";
-    public static final String USERNAME_YA_EXISTE = "Username ya existe";
+    public static final String EMAIL_YA_EXISTE = "El correo electronico ya esta registrado";
+    public static final String USERNAME_YA_EXISTE = "El nombre de usuario ya existe";
     public static final String NO_USER_FOUND_BY_EMAIL = "No se encontro el usuario por correo";
 }
