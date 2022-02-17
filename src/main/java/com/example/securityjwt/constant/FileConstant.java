ed70002e0a11050c4c6611383c8cf12409667cef
GersonMurguia <aldmurguia1@gmail.com>
2022-02-17 13:02:41 -0500

    Arreglado error de escritura en constante DOT

--- src/main/java/com/example/securityjwt/constant/FileConstant.java
+++ src/main/java/com/example/securityjwt/constant/FileConstant.java
@@ -7,7 +7,7 @@ public class FileConstant {
     public static final String DIRECTORY_CREATED="Directorio creado para:";
     public static final String DEFAULT_USER_IMAGE_PATH="/user/image/profile/";
     public static final String FILE_SAVED_IN="Archivo guardado en el sistema con el nombre: ";
-    public static final String DOT="";
+    public static final String DOT=".";
     public static final String FORWARD_SLASH="/";
     public static final String TEMP_PROFILE_IMAGE_BASE_URL="https://robohash.org/";
 
